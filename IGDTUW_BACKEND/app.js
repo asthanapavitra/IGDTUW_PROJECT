c5485c80f97ce0a9864ad980938f1bb2be5e4cb2
Ruchi3105 <ruchi.cse31@gmail.com>
2025-03-21 23:47:13 +0530

    register frontend done

--- IGDTUW_BACKEND/app.js
+++ IGDTUW_BACKEND/app.js
@@ -15,7 +15,7 @@ const port=process.env.PORT||5000;
 
 app.use(cookie());
 app.use(express.json());
-app.use(cors());
+app.use(cors({ origin: "http://localhost:5173", credentials: true }));
 app.use(express.urlencoded({extended:true}));
 app.use('/student',studentRouter);
 app.listen(port,()=>{
