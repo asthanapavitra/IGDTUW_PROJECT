d5db942b4fbd29436210a62a53329a0965d455b0
Ruchi3105 <ruchi.cse31@gmail.com>
2025-05-27 18:37:53 +0530

    cors origin updated

--- IGDTUW_BACKEND/app.js
+++ IGDTUW_BACKEND/app.js
@@ -19,7 +19,13 @@ const port=process.env.PORT||5000;
 
 app.use(cookie());
 app.use(express.json());
-app.use(cors());
+
+app.use(cors({
+  origin: process.env.CORS_ORIGIN,
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  credentials: true // if you're using cookies/auth headers
+}));
+
 app.use(session({
     secret: process.env.SESSION_SECRET,
     resave: false,
